4ae48bd8b15302eea56799043a9d2f69374d1258
charlieWanlin <charliewanlin.pro@gmail.com>
2026-08-31 16:40:33 +0200

    chore(front): initialisation Angular 22, Tailwind, ESLint et Prettier

---
+++ frontend/eslint.config.js
@@ -0,0 +1,51 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+      // Doit rester en DERNIER : désactive les règles ESLint qui gèrent le formatage
+      // (indentation, guillemets, points-virgules...) pour laisser Prettier s'en occuper.
+      eslintConfigPrettier,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      // Une variable prefixee par _ est volontairement inutilisee
+      // (ex. retirer une propriete par destructuration : { ts: _ts, ...reste }).
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {},
+  },
+]);
